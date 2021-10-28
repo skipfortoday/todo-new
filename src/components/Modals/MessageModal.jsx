4dd15b134af1e5d79cf8fe15f3b0120cb5ebe981
Rizqi Pratama <43211197+skipfortoday@users.noreply.github.com>
2021-10-28 07:33:01 +0700

    Add files via upload

---
+++ src/components/Modals/MessageModal.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+import { ReactComponent as warningIcon } from "../../assets/img/warning.svg";
+import { ReactComponent as infoIcon } from "../../assets/img/info.svg";
+
+const MessageModal = ({ show, onHide, msg, type, ...props }) => {
+  const IconStatus = type !== 0 ? infoIcon : warningIcon;
+
+  return (
+    <Modal
+      {...{ show, onHide, ...props }}
+      animation={false}
+      dialogClassName="modal-todo-information"
+      aria-labelledby="contained-modal-title-vcenter"
+      data-cy="modal-information"
+      size="lg"
+      centered
+    >
+      <Modal.Body>
+        <div data-cy="modal-information-icon">
+          <IconStatus />
+        </div>
+
+        <div
+          className="modal-todo-information__title"
+          data-cy="modal-information-title"
+        >
+          {msg}
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default React.memo(MessageModal);
